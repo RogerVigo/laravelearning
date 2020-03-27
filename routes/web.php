4b9e56ac41231c05aead37f95ae92806fb21f8fd
CM-CA <manuel.couseloalonso@gmail.com>
2020-03-27 19:06:02 +0100

    post lesson and rearrenge code

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,30 @@ Route::get('test', function (){
 //Pasar parametro desde la barra de navegador
 Route::get('/test/test-1',function (){
 
-    return view('test',[
-        'name'=>request('name')
+    return view('test', [
+        'name' => request('name')
     ]);
 });
 
 //Acceso a los valores de configuracion
-Route::get('/time',function (){
+Route::get('/time', function () {
     return config('app.timezone');
+});
+
+//Mostrar datos de un array
+
+//Los datos se recogen con la wildcard y se pasan como parametro de la funcion
+Route::get('/post/{post}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Here is my First post',
+        'my-second-post' => 'This is my second post'
+    ];
+//Si no encuentra la key correspondiente, mostrara un error 404
+    if (!array_key_exists($post, $posts)) {
+        abort(404, 'No post was found');
+    }
+//Si encuentra la key correspondiente, abrira una vista con el contenido
+    return view('post', [
+        'post' => $post]);
+
 });
