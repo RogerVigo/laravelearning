785eb1c9c44f1fa0800aa4c3583c7af96d443c9c
CM-CA <manuel.couseloalonso@gmail.com>
2020-04-07 22:40:19 +0200

    Laracast-7-Eloquent Create new migrations and filling BDD using faker. Some problems on BDD structure when i create a new entry of the blog. Error-> "General error: 1364 Field 'user_id' doesn't have a default value". Check the controller "ArticlesController.php" for more info in coment.

--- app/Article.php
+++ app/Article.php
@@ -7,10 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'excerpt', 'body']; //Si te da igual lo que se vaya a guardar, dejar un array vacio.
-    /**
-     * @var int
-     */
-    public $user_id;
 
     function path()
     {
