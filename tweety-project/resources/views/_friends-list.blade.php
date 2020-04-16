517014d1e6df8e55f22b49634e4e081047c82b4a
CM-CA <manuel.couseloalonso@gmail.com>
2020-04-17 00:07:26 +0200

    Laracast-Final-Project-3. Add new migration pivot table. We can see the peoplethat are we following. Change time zone to Europe/Madrid

--- tweety-project/resources/views/_friends-list.blade.php
+++ tweety-project/resources/views/_friends-list.blade.php
@@ -2,12 +2,13 @@
     <h3 class="font-bold text-xl mb-4">Following</h3>
 
     <ul>
-        @foreach(range(1,8) as $index)
+        @foreach(auth()->user()->follows as $user)
             <li class="mb-4">
                 <div class="flex items-center text-sm">
-                    <img src="https://i.pravatar.cc/40"
+                    <img src="{{$user->avatar}}"
                          alt=""
-                         class="rounded-b-full mr-2"> Jhon Doe
+                         class="rounded-b-full mr-2">
+                    {{$user->name}}
                 </div>
             </li>
         @endforeach
